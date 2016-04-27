f3e791f82f1398473b6d675d582d62ae22f0cb11
Kyle Briggs <kbrig035@uottawa.ca>
2016-04-27 16:42:23 -0400

    added GPL to bessel.c

--- bessel.c
+++ bessel.c
@@ -1,3 +1,27 @@
+/*
+
+                                COPYRIGHT
+    Copyright (C) 2015 Kyle Briggs (kbrig035<at>uottawa.ca), and is based on
+    the scipy.signal implementation of bessel
+
+    This file is part of CUSUM.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
+
 #include"bessel.h"
 
 void besselap(uint64_t N, double complex *poles, double complex *zeros)
