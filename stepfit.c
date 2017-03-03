07b42cd4b6a3d328cf7d540d42e21260a0be63f2
Kyle Briggs <kbrig035@uottawa.ca>
2017-03-03 15:39:54 -0500

    more precision in debug print

--- stepfit.c
+++ stepfit.c
@@ -137,7 +137,7 @@ void step_response(event *current, double risetime, int64_t maxiters, double min
         {
             FILE *errors = fopen64_and_check("errors.csv","a",111);
             current->type = FITSTEP;
-            fprintf(errors,"%"PRId64",%"PRId64",%g,%g,%g\n",current->index, current->start, a, b, minstep);
+            fprintf(errors,"%"PRId64",%"PRId64",%.15g,%.15g,%.15g\n",current->index, current->start, a, b, minstep);
             fclose(errors);
             free(time);
             return;
